76b56be81c0d5fadf58a8866858db1793a335de0
vedret.hrvanovic@gmail.com <wedretwedret>
2018-06-28 09:57:21 +0200

    Scroll to Bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,23 @@
 
         let socket=io();
 
+        function scrollToBottom () {
+            // Selectors
+            var messages = jQuery('#messages');
+            var newMessage = messages.children('li:last-child')
+            // Heights
+            var clientHeight = messages.prop('clientHeight');
+            var scrollTop = messages.prop('scrollTop');
+            var scrollHeight = messages.prop('scrollHeight');
+            var newMessageHeight = newMessage.innerHeight();
+            var lastMessageHeight = newMessage.prev().innerHeight();
+          
+            if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+              messages.scrollTop(scrollHeight);
+            }
+          }
+
+          
         socket.on('connect',function(){
         console.log('Connected to server')
 
@@ -25,13 +42,8 @@
         });
 
         jQuery('#messages').append(html);
+        scrollToBottom();
 
-        //   let formatedTime = moment(message.createdAt).format('h:mm a');
-
-        //   let li = jQuery('<li></li>');
-        //   li.text(`${message.from} ${formatedTime}: ${message.text}`);
-
-        //   jQuery('#messages').append(li);
       })
 
       socket.on('newLocationMessage',function(message){
@@ -49,6 +61,8 @@
 
    
        jQuery('#messages').append(html);
+       scrollToBottom();
+
       })
 
       jQuery('#message-form').on('submit',function(e){
